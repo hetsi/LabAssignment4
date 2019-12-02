dbc48be78b91d91d027eddadd1966b3561ff4b4c
hetsi <hetsi.shah138@gmail.com>
2019-12-02 00:55:29 -0800

    Adding GradeTracker class

---
+++ src/labassignment/GradeTracker.java
@@ -0,0 +1,31 @@
+package labassignment;
+
+public class GradeTracker {
+
+	private String calculateGrade(Student student) {
+		String grade;
+		double Average = new OperationCalculateAverage().calculateClassAverage(student);
+		
+		if(Average>=90) {
+			grade ="A";
+			
+		}else if (Average >= 80) {
+			grade = "B";
+			
+		}else if (Average >= 70) {
+			grade = "C";
+			
+		}else if (Average >= 60) {
+			grade = "D";
+			
+		}else {
+			grade = "F";
+		}
+		
+		return grade;
+	}
+	
+	public void printGrade(Student student) {
+		System.out.println("Grade of the student : " + student.name+ " = " + calculateGrade(student));
+	}
+}
